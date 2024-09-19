579185e95b53e1d6ffb58b8cda7b31b825bb79d5
ChrisDav05 <davioteotonio05@gmail.com>
2024-09-18 23:01:16 -0300

    Envio das atividades de testes automatizados

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,66 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
 
+describe ("Testando classe ContaBancaria",() =>{
+    let conta:ContaBancaria
+    let contaDestino: ContaBancaria
+    beforeEach(() => {
+    conta = new ContaBancaria()
+    contaDestino = new ContaBancaria()
+    
+});
 
+test("Testando depositar valor válido", () => {
+    conta.depositar(100);
+    expect(conta.consultarSaldo()).toBe(100);
+  });
+
+test("Testando depositar valor inválido", () => {
+    conta.depositar(-50);
+    expect(conta.consultarSaldo()).toBe(0);
+  });
+
+test("Testando sacar valor válido", () => {
+    conta.depositar(100);
+    conta.sacar(10)
+    expect(conta.consultarSaldo()).toBe(90);
+  });
+
+test("Testando sacar valor inválido", () => {
+    conta.depositar(100)
+    conta.sacar(120)
+    expect(conta.consultarSaldo()).toBe(100);
+});
+
+test("Testando transferência válida", () => {
+  conta.depositar(100);
+  conta.transferir(10,contaDestino);
+  expect(conta.consultarSaldo()).toBe(90);
+  expect(contaDestino.consultarSaldo()).toBe(10);
+});
+
+test("Testando transferência inválida", () => {
+  conta.depositar(100)
+  conta.transferir(-10,contaDestino);
+  expect(conta.consultarSaldo()).toBe(100);
+});
+
+test("Consultando o saldo", () => {
+  conta.consultarSaldo();
+});
+
+test("Exibição de extrato", () => {
+  conta.depositar(100);
+  conta.sacar(10);
+  let extrato = conta.exibirExtrato();
+  expect(extrato.length).toBe(2);
+})
+
+test("Registro de operações no extrato", () => {
+  conta.depositar(200)
+  conta.transferir(100,contaDestino);
+  let extrato = conta.exibirExtrato();
+  expect(extrato[2].descricao).toBe(`Transferência de 100 para a conta ${contaDestino}`);
+})
+
+})
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,47 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    private numeroConta = 1000;
+    private agencia = 100;
+    private saldo = 0;
+    private extrato: { data: string, descricao: string }[] = [];
+
+public depositar(valor: number) {
+    if (valor > 0) {
+      this.saldo += valor;
+      this.registrarOperacao(`Depósito no valor ${valor}`)
+    }
+}
+
+public consultarSaldo() {
+    return this.saldo;
+}
+
+public sacar(valor:number){
+    if (this.saldo >= valor){
+        this.saldo -= valor
+        this.registrarOperacao(`Saque no valor de ${valor}`)
+    } else {
+      throw new Error(`Saldo insuficiente ou valor inválido`)
+    }
+}
+
+public transferir(valor:number, contaDestino:ContaBancaria){
+    if (this.saldo >= valor && valor > 0){
+      this.sacar(valor);
+      contaDestino.depositar(valor);
+      this.registrarOperacao(`Transferência de ${valor} para a conta ${contaDestino}`)
+    } else {
+      throw new Error('Saldo insuficiente ou valor inválido');
+    }
+}
+
+public exibirExtrato(){
+  return this.extrato
+}
+
+public registrarOperacao(descricao:string){
+   let data = new Date().toLocaleString();
+   this.extrato.push({data, descricao});
+}
+
+
+}
